a96d852969b17a780da388b662da0c5fc8e13d34
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-09 10:47:03 +0530

    Done with setup of routing and Pages through the website

--- React_Practice/Whatsapp-Clone/src/App.jsx
+++ React_Practice/Whatsapp-Clone/src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <>
     <h1>Whatsapp-Clone</h1>
+    <Routes>
+      <Route path = "/home" element={<Home />}></Route>
+      <Route path = "/login" element={<Login />}></Route>
+      <Route path = "/chat/:chatId" element={<Chat />}></Route>
+      <Route path = "*" element={<NotFound />}></Route>
+    </Routes>
     </>
   );
 }
---
+++ React_Practice/Whatsapp-Clone/src/pages/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <>
+    <h2>Chat Screen</h2>
+    </>
+  )
+}
+
+export default Chat
